3f212db02525c5f18e6d6dbc5219865c0ee101a9
Глеб <varnayev.pavlodar@gmail.com>
2022-03-05 20:54:53 +0300

    Add hiny for number entering

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ and merged via pull request to master.
 """
 
 if __name__ == "__main__":
-    a = float(input())
-    b = float(input())
+    a = float(input("Enter first number: "))
+    b = float(input("Enter second number: "))
     print(f'The sum is {sum_func(a, b)}')
     print(f'The difference is {subtract_func(a, b)}')
